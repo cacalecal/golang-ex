c70b464fff6c7b19d9dafd5133ee8bb946dd5dc4
cacalecal <61261077+cacalecal@users.noreply.github.com>
2020-02-20 11:08:08 +0700

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift! coba aja"
+		response = "Hello OpenShift! coba a"
 	}
 
 	fmt.Fprintln(w, response)
